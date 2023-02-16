3b196c2beab5fcb33cc601d65e1fc5a10f3fa2ac
Anna <mrs.anna.ozolina@gmail.com>
2023-02-16 18:02:52 +0200

    updates

--- RockPaperScissors/src/main.tsx
+++ RockPaperScissors/src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import { BrowserRouter as Router } from "react-router-dom";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query"
-import './index.css'
 
 import "./i18n"
 
